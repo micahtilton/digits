ce8e0542ab0fa0b9bddb29841f12ddce9f341d96
micahtilton <micah.tilton.20@gmail.com>
2023-10-27 19:00:08 -1000

    wod done 6 minutes

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,20 +1,32 @@
 import React from 'react';
-import { Col, Container, Image, Row } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
+import { FileEarmarkTextFill, PeopleFill, Calendar2CheckFill } from 'react-bootstrap-icons';
 
 /* A simple static component to render some text for the landing page. */
 const Landing = () => (
   <Container id="landing-page" fluid className="py-3">
-    <Row className="align-middle text-center">
-      <Col xs={4}>
-        <Image roundedCircle src="/images/meteor-logo.png" width="150px" />
-      </Col>
+    <Container>
+      <Row className="align-middle text-center">
 
-      <Col xs={8} className="d-flex flex-column justify-content-center">
-        <h1>Welcome to this template</h1>
-        <p>Now get to work and modify this app!</p>
-      </Col>
+        <Col className="d-flex flex-column align-items-center">
+          <PeopleFill size={100} />
+          <h1>Multiple Users</h1>
+          <p>This address book enables any number of users to register and save their business contacts. You can only see the contacts you have created.</p>
+        </Col>
+        <Col className="d-flex flex-column align-items-center">
+          <FileEarmarkTextFill size={100} />
+          <h1>Contact Details</h1>
+          <p>This address book enables any number of users to register and save their business contacts. You can only see the contacts you have created.</p>
+        </Col>
+        <Col className="d-flex flex-column align-items-center">
+          <Calendar2CheckFill size={100} />
+          <h1>Timestamped Notes</h1>
+          <p>This address book enables any number of users to register and save their business contacts. You can only see the contacts you have created.</p>
+        </Col>
+      </Row>
 
-    </Row>
+    </Container>
+
   </Container>
 );
 
